0427e540b512963f56c1cc677d6475827aff90b3
rexvel <rexvelele@gmail.com>
2024-10-02 20:02:19 +0300

    feat: usage of crypto library in browser

---
+++ crypto-browser-api.js
@@ -0,0 +1,6 @@
+// crypto library is supported in browsers
+// https://developer.mozilla.org/en-US/docs/Web/API/Crypto
+
+// seen at https://x.com/mattpocockuk/status/1841399224518287784
+
+console.log(crypto.randomUUID());
