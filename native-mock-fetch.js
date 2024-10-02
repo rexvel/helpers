15590eab32fe72d6bd5745339bebf585eccf3f45
rexvel <rexvelele@gmail.com>
2024-10-02 19:46:21 +0300

    feat: add undici mocked fetch request

---
+++ native-mock-fetch.js
@@ -0,0 +1,25 @@
+import { MockAgent, setGlobalDispatcher } from "undici";
+
+const mockAgent = new MockAgent();
+setGlobalDispatcher(mockAgent);
+
+// 3. Mock a certain URL
+const mockPool = mockAgent.get("http://localhost:3000");
+
+// 4. Mark the URL to intercept, and the reply to send back
+mockPool
+  .intercept({
+    path: "/me",
+    method: "GET",
+  })
+  .reply(200, {
+    id: "123",
+    name: "Matt",
+  });
+
+// 5. Any fetch requests to the URL will now be intercepted!
+fetch("http://localhost:3000/me")
+  .then((res) => res.json())
+  .then((user) => {
+    console.dir(user); // { id: '123', name: 'Matt' }
+  });
